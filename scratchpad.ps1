7d9abfbefcc0de2e787a41226dfccf20c173f16c
David Boulet <dboulet@datahiveanalytics.com>
2019-03-16 08:50:36 -0400

    reformatted

--- scratchpad.ps1
+++ scratchpad.ps1
@@ -6,42 +6,46 @@ $param = @{
                 "10.0.0.0/16"
                 "172.16.0.0/16"
             )
-            subnets = @(@{
-                name = "firstSubnet"
-                properties = @{
-                    addressPrefix = "10.0.1.0/24"
+            subnets = @(
+                @{
+                    name = "firstSubnet"
+                    properties = @{
+                        addressPrefix = "10.0.1.0/24"
+                    }
+                },
+                @{
+                    name = "secondSubnet"
+                    properties = @{
+                        addressPrefix = "10.0.2.0/24"
+                    }
+                },
+                @{
+                    name = "thirdSubnet"
+                    properties = @{
+                        addressPrefix = "172.16.1.0/24"
+                    }
                 }
-            },
-            @{
-                name = "secondSubnet"
-                properties = @{
-                    addressPrefix = "10.0.2.0/24"
-                }
-            },
-            @{
-                name = "thirdSubnet"
-                properties = @{
-                    addressPrefix = "172.16.1.0/24"
-                }
-            }
-        )},
+            )
+        },
         @{
             name = "TestVnet2"
             addressPrefixes = @(
                 "192.168.0.0/16"
             )
-            subnets = @(@{
-                name = "firstSubnet"
-                properties = @{
-                    addressPrefix = "192.168.1.0/24"
+            subnets = @(
+                @{
+                    name = "firstSubnet"
+                    properties = @{
+                        addressPrefix = "192.168.1.0/24"
+                    }
+                },
+                @{
+                    name = "secondSubnet"
+                    properties = @{
+                        addressPrefix = "192.168.2.0/24"
+                    }
                 }
-            },
-            @{
-                name = "secondSubnet"
-                properties = @{
-                    addressPrefix = "192.168.2.0/24"
-                }
-            })
+            )
         }
     )
 }
