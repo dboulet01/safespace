2b98097228eea5f5171785494843a08898ea6b12
David Boulet <dboulet@datahiveanalytics.com>
2019-03-16 21:02:01 -0400

    edits

---
+++ vnetDeploy.ps1
@@ -0,0 +1,44 @@
+function New-AzureVnet {
+    [CmdletBinding()]
+    param (
+        #Target Subscription - will accept subscription name or subscription Id
+        [Parameter()]
+        [String]
+        $Subscription,
+
+        #Resource Group
+        [Parameter()]
+        [String]
+        $ResourceGroup
+
+    )
+    
+    begin {
+
+        Write-Host "Checking if Az module is installed." -ForegroundColor Yellow
+        $module = Get-Module Az* -ListAvailable
+
+        if(!$module){
+            Write-Host "Az module not found.. Installing Az module." -ForegroundColor Yellow
+            Install-Module Az -AllowClobber -Force
+        } else {
+            Write-Host "Az module found." -ForegroundColor Green
+        }
+
+        Write-Host "Setting Azure context." -ForegroundColor Yellow
+        if($Subscription){
+            Set-AzContext -Subscription $Subscription
+        }
+        else {
+            Write-Host "Subscription not provided - using current context." -ForegroundColor Yellow
+            Get-AzContext
+        }
+
+    }
+    
+    process {
+    }
+    
+    end {
+    }
+}
